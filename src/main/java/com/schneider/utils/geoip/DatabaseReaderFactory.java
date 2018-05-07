a146e5dddad12b43e4e2ab61008c62fa47e87985
Donald McDougal <donald.mcdougal@gmail.com>
2018-05-07 15:25:36 -0500

    Implemented the DatabaseReaderFactory factory class.

---
+++ src/main/java/com/schneider/utils/geoip/DatabaseReaderFactory.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.schneider.utils.geoip;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.maxmind.geoip2.DatabaseReader;
+
+/**
+ * A factory for generating GeoIP2 database readers.
+ * @author John Schneider
+ *
+ */
+public class DatabaseReaderFactory {
+
+	/**
+	 * Creates a new GeoIP2 DatabaseReader.
+	 * @param file The file to use for the database.
+	 * @return A new DatabaseReader object.
+	 * @throws IOException
+	 */
+	public static DatabaseReader createDatabaseReader(File file) throws IOException {
+		DatabaseReader reader = new DatabaseReader.Builder(file).build();
+		return reader;
+	}
+}
